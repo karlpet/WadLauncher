57cfaade4743f28d0264508ec5f0bfaeaf84ce26
Karl Petter Svensson <karlpet@users.noreply.github.com>
2020-11-11 04:42:45 +0100

    Added .pk3 file extension to loader. (#21)

--- src/main/python/models/Wads.py
+++ src/main/python/models/Wads.py
@@ -3,12 +3,23 @@ import os
 from models import Model
 from config import Config
 
+
 def wad_loader():
     config = Config.Instance()
     wads_path = os.path.expanduser(config['PATHS']['WADS_PATH'])
-    
-    return [{ 'id': str(index), 'name': dir, 'file': dir + '.wad'} for index, dir in enumerate(os.listdir(wads_path))
-                if os.path.isdir(os.path.join(wads_path, dir))]
+    extensions = ['.wad', '.WAD', '.pk3']
+
+    def load_wad(index, dir):
+        print(dir)
+
+        for file in os.listdir(os.path.join(wads_path, dir)):
+            if any((file.endswith(ext) for ext in extensions)):
+                return { 'id': str(index), 'name': dir, 'file': file }
+        
+        return { 'id': str(index), 'name': 'ERROR, no wad found!', 'file': '' }
+
+    return [load_wad(index, dir) for index, dir in enumerate(os.listdir(wads_path))
+                                 if os.path.isdir(os.path.join(wads_path, dir))]
 
 class Wads(Model):
     def __init__(self):
