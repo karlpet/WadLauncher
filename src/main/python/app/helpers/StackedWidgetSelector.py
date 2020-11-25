6c4d8f74b80e9086def534e70166c4cbf0bdd082
karlpet <karlpetter.svensson@hotmail.se>
2020-11-25 01:13:30 +0100

    Added welcome screen.

--- src/main/python/app/helpers/StackedWidgetSelector.py
+++ src/main/python/app/helpers/StackedWidgetSelector.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import QStackedWidget
 class WidgetIndices(enum.Enum):
     IDGAMES_SEARCH = 0
     IDGAMES_DETAIL = 1
+    WELCOME = 2
 
 def display_widget(root, widget_index):
     stackedWidget = root.findChild(QStackedWidget, 'main_stack')
