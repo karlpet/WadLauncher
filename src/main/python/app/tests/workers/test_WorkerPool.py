61f8e796121327a70f12a380ef52857dbf583db8
karlpet <karlpetter.svensson@hotmail.se>
2020-11-26 03:05:21 +0100

    Updated workerpool testing.

--- src/main/python/app/tests/workers/test_WorkerPool.py
+++ src/main/python/app/tests/workers/test_WorkerPool.py
@@ -1,21 +1,4 @@
-from core.utils.decorators import Singleton
-
-@Singleton
-class WorkerPool:
-    def __init__(self):
-        self.pool = {}
-        self.worker_id = 0
-    
-    def start(self, worker):
-        self.worker_id += 1
-        worker_id = self.worker_id
-        self.pool[worker_id] = worker
-        worker.finished.connect(lambda: self.remove(worker_id))
-        worker.start()
-    
-    def remove(self, worker_id):
-        self.pool.pop(worker_id)
-
+from app.workers.WorkerPool import *
 
 class MockSignal():
     def __init__(self):
@@ -30,7 +13,6 @@ class MockWorker():
     def start(self):
         for func in self.finished.funcs:
             func()
-            
 
 def test_happy_path():
     pool = WorkerPool.Instance()
