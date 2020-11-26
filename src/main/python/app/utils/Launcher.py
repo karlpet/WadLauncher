b5f7b7f2e801b09c92da6fbc18a342ed590a1ef1
karlpet <karlpetter.svensson@hotmail.se>
2020-11-26 08:27:15 +0100

    Fix wad loader to search wad dir recursively for .wad or .pk3 file.

--- src/main/python/app/utils/Launcher.py
+++ src/main/python/app/utils/Launcher.py
@@ -8,11 +8,10 @@ def launch(wad, iwad, source_port):
 
     wads_path = os.path.expanduser(config['PATHS']['WADS_PATH'])
     wad_dir = os.path.join(wads_path, wad['name'])
-    wad_file_path = os.path.join(wad_dir, wad['file'])
 
     wad_save_dir = os.path.join(wad_dir, 'saves')
     Path(wad_save_dir).mkdir(parents=True, exist_ok=True)
 
-    process_call = source_port['template'].format(wad=wad_file_path, iwad=iwad['path'], save_dir=wad_save_dir)
+    process_call = source_port['template'].format(wad=wad['file_path'], iwad=iwad['path'], save_dir=wad_save_dir)
 
     subprocess.Popen(process_call.split(' '))
