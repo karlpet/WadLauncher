c52f65ff361f46c69f42e57fac69cceb8989a2d8
karlpet <karlpetter.svensson@hotmail.se>
2020-11-29 04:35:04 +0100

    Searchbar will now focus on selection.

--- src/main/python/app/views/widgets/SearchBar.py
+++ src/main/python/app/views/widgets/SearchBar.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QPushButton, QPushButton, QComboBox, QLineEdit
 
 from app.views.viewmodels.ComboBoxModel import ComboBoxModel
 from app.workers.DWApiWorker import SEARCH_TYPES
+from app.helpers.StackedWidgetSelector import widget_changed, WidgetIndices
 
 class SearchBar:
     def __init__(self, root, controller):
@@ -12,7 +13,13 @@ class SearchBar:
         self.searchbar_input = root.findChild(QLineEdit, 'idgames_searchbar_input')
 
         self.searchbar_search.clicked.connect(self.search)
+        self.searchbar_input.returnPressed.connect(self.search)
         self.searchbar_type_selector.setModel(ComboBoxModel(SEARCH_TYPES))
+        widget_changed(root, self.on_widget_change)
+
+    def on_widget_change(self, widget_index):
+        if widget_index == WidgetIndices.IDGAMES_SEARCH:
+            self.searchbar_input.setFocus()
 
     def search(self):
         text = self.searchbar_input.text()
