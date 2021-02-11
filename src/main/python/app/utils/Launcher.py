777a1ad56ab6f62dfb076a0c6a7fa1bd402a2ec1
Karl Petter Svensson <karlpet@users.noreply.github.com>
2021-02-11 07:19:14 +0100

    Make user able to only launch the base game iwad. (#103)
    Co-authored-by: karlpet <karlpetter.svensson@gmail.com>

--- src/main/python/app/utils/Launcher.py
+++ src/main/python/app/utils/Launcher.py
@@ -7,21 +7,31 @@ def launch(base_wad, files, iwad, source_port):
     config = Config.Instance()
 
     wads_path = os.path.expanduser(config['PATHS']['WADS_PATH'])
-    wad_dir = os.path.join(wads_path, base_wad['name'])
-
-    wad_save_dir = os.path.join(wad_dir, 'saves')
-    Path(wad_save_dir).mkdir(parents=True, exist_ok=True)
-
     executable_path = os.path.join(source_port['dir'], source_port['executable'])
+
+    files_arg_with_params = []
+    if len(files) > 0:
+        files_arg_with_params = [source_port['wad_arg'], *files]
+
+    iwad_arg_with_params = []
+    if iwad:
+        iwad_arg_with_params = [source_port['iwad_arg'], iwad['path']]
+
+    savedir_arg_with_params = []
+    if base_wad and len(files) > 0:
+        wad_dir = os.path.join(wads_path, base_wad['name'])
+        wad_save_dir = os.path.join(wad_dir, 'saves')
+        Path(wad_save_dir).mkdir(parents=True, exist_ok=True)
+        savedir_arg_with_params = [
+            source_port['save_arg'],
+            wad_save_dir
+        ]
 
     process_call = [
         executable_path,
-        source_port['wad_arg'],
-        *files,
-        source_port['iwad_arg'],
-        iwad['path'],
-        source_port['save_arg'],
-        wad_save_dir,
+        *files_arg_with_params,
+        *iwad_arg_with_params,
+        *savedir_arg_with_params,
     ]
 
     subprocess.Popen(process_call, cwd=source_port['dir'])
