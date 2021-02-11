cf0d06794677ee858ef03cafa9f0defdc7af6e1d
karlpet <karlpetter.svensson@gmail.com>
2021-02-11 07:32:00 +0100

    Set savegame location based on dir of first file in file_paths supplied, instead of some base wad.

--- src/main/python/app/utils/Launcher.py
+++ src/main/python/app/utils/Launcher.py
@@ -3,29 +3,29 @@ from pathlib import Path
 
 from app.config import Config
 
-def launch(base_wad, files, iwad, source_port):
+def launch(files, iwad, source_port):
     config = Config.Instance()
 
     wads_path = os.path.expanduser(config['PATHS']['WADS_PATH'])
     executable_path = os.path.join(source_port['dir'], source_port['executable'])
 
     files_arg_with_params = []
+    savedir_arg_with_params = []
     if len(files) > 0:
         files_arg_with_params = [source_port['wad_arg'], *files]
 
-    iwad_arg_with_params = []
-    if iwad:
-        iwad_arg_with_params = [source_port['iwad_arg'], iwad['path']]
-
-    savedir_arg_with_params = []
-    if base_wad and len(files) > 0:
-        wad_dir = os.path.join(wads_path, base_wad['name'])
+        wad_dir = os.path.dirname(os.path.abspath(files[0]))
         wad_save_dir = os.path.join(wad_dir, 'saves')
         Path(wad_save_dir).mkdir(parents=True, exist_ok=True)
         savedir_arg_with_params = [
             source_port['save_arg'],
             wad_save_dir
         ]
+
+    iwad_arg_with_params = []
+    if iwad:
+        iwad_arg_with_params = [source_port['iwad_arg'], iwad['path']]
+
 
     process_call = [
         executable_path,
